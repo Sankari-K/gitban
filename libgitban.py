f3592f6092ca5e93a9db175ec1779000dfcda09d
Sankari Karthik <sankarikarthik03@gmail.com>
2024-10-23 08:14:51 +0530

    Reading and writing objects

--- libgitban.py
+++ libgitban.py
@@ -22,6 +22,37 @@ argsp.add_argument("path",
                    default=".",
                    help="Where to create the repository.")
 
+argsp = argsubparsers.add_parser("cat-file",
+                                 help="Provide content of repository objects")
+
+argsp.add_argument("type",
+                   metavar="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   help="Specify the type")
+
+argsp.add_argument("object",
+                   metavar="object",
+                   help="The object to display")
+
+argsp = argsubparsers.add_parser(
+    "hash-object",
+    help="Compute object ID and optionally creates a blob from a file")
+
+argsp.add_argument("-t",
+                   metavar="type",
+                   dest="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   default="blob",
+                   help="Specify the type")
+
+argsp.add_argument("-w",
+                   dest="write",
+                   action="store_true",
+                   help="Actually write the object into the database")
+
+argsp.add_argument("path",
+                   help="Read object from <file>")
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
 
@@ -268,3 +299,42 @@ def object_write(obj, repo=None):
                 # Compress and write
                 f.write(zlib.compress(result))
     return sha
+
+def object_find(repo, name, fmt=None, follow=True):
+    return name
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+def object_hash(fd, fmt, repo=None):
+    """ Hash object, writing it to repo if provided."""
+    data = fd.read()
+
+    # Choose constructor according to fmt argument
+    if fmt == b'commit':
+        obj = GitCommit(data)
+    elif fmt == b'tree':
+        obj = GitTree(data)
+    elif fmt == b'tag':
+        obj = GitTag(data)
+    elif fmt == b'blob':
+        obj = GitBlob(data)
+    else:
+        raise Exception("Unknown type %s!" % fmt)
+
+    return object_write(obj, repo)
